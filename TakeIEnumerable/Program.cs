2c59adb732ea75cf931b2054867b530b6fe8e8a1
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-07-04 23:52:58 +0200

    Add files via upload

---
+++ TakeIEnumerable/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TakeIEnumerable
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var result = arr.Take(5);
+            foreach (int i in result)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
